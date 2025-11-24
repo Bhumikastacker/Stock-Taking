6f64e648e272e46b1984602ddf7d993c79ba9feb
Bhumika Dixit <bhumika.d@stackerbee.com>
2025-11-24 17:39:16 +0530

    stock Taking

---
+++ stock_taking/stock_taking/doctype/stock_taking/test_stock_taking.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, bhumika.d@stackerbee.com and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestStockTaking(FrappeTestCase):
+	pass
---
+++ stock_taking/stock_taking/doctype/stock_taking_warehouse/stock_taking_warehouse.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, bhumika.d@stackerbee.com and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class stocktakingWarehouse(Document):
+	pass
