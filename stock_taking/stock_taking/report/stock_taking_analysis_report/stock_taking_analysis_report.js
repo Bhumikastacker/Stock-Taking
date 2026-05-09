627990def0691eba9b4f6c612db2fd6fa35476ac
Arpit Panchal <Arpit.panchal@stackerbee.com>
2026-05-09 16:31:28 +0530

    stock taking report

---
+++ stock_taking/stock_taking/report/stock_taking_analysis_report/stock_taking_analysis_report.js
@@ -0,0 +1,51 @@
+frappe.query_reports["Stock Taking Analysis Report"] = {
+
+    filters: [
+
+        {
+            fieldname: "from_date",
+            label: __("From Date"),
+            fieldtype: "Date",
+            width: 120
+        },
+
+        {
+            fieldname: "to_date",
+            label: __("To Date"),
+            fieldtype: "Date",
+            width: 120
+        },
+
+        {
+            fieldname: "company",
+            label: __("Company"),
+            fieldtype: "Link",
+            options: "Company",
+            width: 180
+        },
+
+        {
+            fieldname: "warehouse",
+            label: __("Warehouse"),
+            fieldtype: "Link",
+            options: "Warehouse",
+            width: 180
+        },
+
+        {
+            fieldname: "item_code",
+            label: __("Item Code"),
+            fieldtype: "Link",
+            options: "Item",
+            width: 180
+        },
+
+        {
+            fieldname: "stock_taking",
+            label: __("Stock Taking"),
+            fieldtype: "Link",
+            options: "Stock Taking",
+            width: 180
+        }
+    ]
+};
